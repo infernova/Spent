591a0f4e4d922f9fe5129f941d383bada80b798c
Jesmond <jesmond@inzenstudio.com>
2019-04-25 16:47:37 +0800

    Transfer commit

--- Spent/Assets/Scripts/DailyExpenditureScrollList.cs
+++ Spent/Assets/Scripts/DailyExpenditureScrollList.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DailyExpenditureScrollList : MonoBehaviour
@@ -12,37 +13,73 @@ public class DailyExpenditureScrollList : MonoBehaviour
     [SerializeField]
     private GameObject mListItemTemplate;
 
+    [SerializeField]
+    private float mTopBorder;
+    [SerializeField]
+    private float mBottomBorder;
+
+    private float mViewportSize;
+
     private List<DailyExpenditureListItem> mItemList = new List<DailyExpenditureListItem>();
     public List<DailyExpenditureListItem> ItemList
     {
         get { return mItemList; }
     }
 
-    private List<GameObject> mCurrDisplay = new List<GameObject>();
+    private List<DailyExpenditureSetItem> mCurrDisplay = new List<DailyExpenditureSetItem>();
 
+    private int mFirstIndex;
     private int mLastIndex;
+
+    private float mTopPos;
     private float mBottomPos;
 
-    // Use this for initialization
+    private List<ExpenditureItem> mItems;
+
+    private bool mIsInit;
+
+    private void Start()
+    {
+        if (mIsInit) return;
+
+        mViewportSize = GetComponent<RectTransform>().rect.height;
+
+        mIsInit = true;
+    }
+
     public void Init (List<ExpenditureItem> items)
     {
+        if (!mIsInit)
+        {
+            Start();
+        }
+
+        mItems = items;
+
         mItemList = new List<DailyExpenditureListItem>();
 
-        foreach(GameObject item in mCurrDisplay)
+        foreach(DailyExpenditureSetItem item in mCurrDisplay)
         {
-            Destroy(item);
+            Destroy(item.gameObject);
         }
+
+        mCurrDisplay = new List<DailyExpenditureSetItem>();
+
+        mContainerRect.anchoredPosition = Vector2.zero;
+
+        mFirstIndex = 0;
+        mTopPos = 0.0f;
 
         mLastIndex = 0;
         mBottomPos = 0.0f;
 
-        while (mLastIndex < items.Count)
+        while (mLastIndex < items.Count && -mBottomPos < mViewportSize)
         {
             GameObject setItem = Instantiate(mListItemTemplate, mContainerRect);
-            setItem.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, mBottomPos);
             float itemHeight = 0.0f;
             List<DailyExpenditureListItem> additionalItems = null;
             setItem.GetComponent<DailyExpenditureSetItem>().LoadExpenditures(items,
+                mBottomPos,
                 ref mLastIndex,
                 out itemHeight,
                 out additionalItems);
@@ -50,9 +87,98 @@ public class DailyExpenditureScrollList : MonoBehaviour
             mItemList.AddRange(additionalItems);
             mBottomPos -= itemHeight;
 
-            mCurrDisplay.Add(setItem);
+            mCurrDisplay.Add(setItem.GetComponent<DailyExpenditureSetItem>());
         }
 
         mContainerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, -mBottomPos);
 	}
+
+    private void Update()
+    {
+        float containerPos = mContainerRect.anchoredPosition.y;
+        mTopBorder = -containerPos;
+        mBottomBorder = -containerPos - mViewportSize;
+
+        if (mLastIndex < mItems.Count && mBottomBorder < mBottomPos)
+        {
+            do
+            {
+                GameObject setItem = Instantiate(mListItemTemplate, mContainerRect);
+                float itemHeight = 0.0f;
+                List<DailyExpenditureListItem> additionalItems = null;
+                setItem.GetComponent<DailyExpenditureSetItem>().LoadExpenditures(mItems,
+                    mBottomPos,
+                    ref mLastIndex,
+                    out itemHeight,
+                    out additionalItems);
+
+                mItemList.AddRange(additionalItems);
+                mBottomPos -= itemHeight;
+
+                mCurrDisplay.Add(setItem.GetComponent<DailyExpenditureSetItem>());
+            } while (mLastIndex < mItems.Count && mBottomBorder < mBottomPos);
+
+            mContainerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, -mBottomPos);
+        }
+
+        while (mLastIndex > 0 && mBottomBorder > mCurrDisplay[mCurrDisplay.Count - 1].TopPos)
+        {
+            DailyExpenditureSetItem set = mCurrDisplay[mCurrDisplay.Count - 1];
+            mLastIndex -= set.NumItems;
+            mBottomPos += set.Size;
+            Destroy(set.gameObject);
+            mCurrDisplay.RemoveAt(mCurrDisplay.Count - 1);
+
+            while (mItemList.Count > mLastIndex)
+            {
+                mItemList.RemoveAt(mItemList.Count - 1);
+            }
+        }
+
+        while (mFirstIndex < mItems.Count && mTopBorder < mCurrDisplay[0].BottomPos)
+        {
+            DailyExpenditureSetItem set = mCurrDisplay[0];
+            mFirstIndex += set.NumItems;
+            mTopPos -= set.Size;
+            Destroy(set.gameObject);
+            mCurrDisplay.RemoveAt(0);
+        }
+
+        while (mFirstIndex > 0 && mTopBorder > mTopPos)
+        {
+            int prevIndex = mFirstIndex - 1;
+            DateTime prevDate = mItems[prevIndex].Date.DateTime.Date;
+
+            while (prevIndex > 0 && mItems[prevIndex - 1].Date.DateTime.Date == prevDate)
+            {
+                prevIndex--;
+            }
+
+            mFirstIndex = prevIndex;
+
+            GameObject setItem = Instantiate(mListItemTemplate, mContainerRect);
+            float itemHeight = 0.0f;
+            List<DailyExpenditureListItem> additionalItems = null;
+            setItem.GetComponent<DailyExpenditureSetItem>().LoadExpenditures(mItems,
+                mTopPos,
+                ref prevIndex,
+                out itemHeight,
+                out additionalItems,
+                true);
+
+            for (int i = 0; i < additionalItems.Count; i++)
+            {
+                mItemList[mFirstIndex + i] = additionalItems[i];
+            }
+
+            mTopPos += itemHeight;
+
+            mCurrDisplay.Insert(0, setItem.GetComponent<DailyExpenditureSetItem>());
+        }
+
+        foreach (DailyExpenditureSetItem set in mCurrDisplay)
+        {
+            set.DoUpdate(mTopBorder);
+        }
+    }
 }
--- Spent/Assets/Scripts/DailyExpenditureSetItem.cs
+++ Spent/Assets/Scripts/DailyExpenditureSetItem.cs
@@ -7,15 +7,44 @@ public class DailyExpenditureSetItem : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI mDateTitle;
+    [SerializeField]
+    private RectTransform mDateTitleContainer;
 
     [SerializeField]
     private GameObject mItemTemplate;
 
+    public float TopPos { get; private set; }
+    public float BottomPos
+    {
+        get { return TopPos - Size; }
+    }
+    public float Size { get; private set; }
+    public int NumItems { get; private set; }
+
+    private float mTitleContainerSize;
+
+    private bool mIsInit;
+
+    private void Start()
+    {
+        if (mIsInit) return;
+
+        mTitleContainerSize = mDateTitleContainer.rect.height;
+        mIsInit = true;
+    }
+
     public void LoadExpenditures(List<ExpenditureItem> list,
+        float topPos,
         ref int index,
         out float size,
-        out List<DailyExpenditureListItem> items)
+        out List<DailyExpenditureListItem> items,
+        bool isReversePos = false)
     {
+        if (!mIsInit)
+        {
+            Start();
+        }
+
         if (index >= list.Count)
         {
             size = 0.0f;
@@ -25,22 +54,56 @@ public class DailyExpenditureSetItem : MonoBehaviour
 
         DateTime selectedDate = list[index].Date;
         float itemHeight = mItemTemplate.GetComponent<RectTransform>().rect.height;
-        int offset = 0;
+        NumItems = 0;
         mDateTitle.SetText(selectedDate.ToString("dddd, d MMM yyyy"));
 
         items = new List<DailyExpenditureListItem>();
    
-        while (index + offset < list.Count && list[index + offset].Date.DateTime.Date == selectedDate.Date)
+        while (index + NumItems < list.Count && list[index + NumItems].Date.DateTime.Date == selectedDate.Date)
         {
             GameObject item = Instantiate(mItemTemplate, transform);
-            item.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, -100.0f - (itemHeight * offset));
-            item.GetComponent<DailyExpenditureListItem>().Init(list[index + offset], index + offset);
+            item.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, -mTitleContainerSize - (itemHeight * NumItems));
+            item.GetComponent<DailyExpenditureListItem>().Init(list[index + NumItems], index + NumItems);
 
             items.Add(item.GetComponent<DailyExpenditureListItem>());
-            offset++;
+            NumItems++;
         }
 
-        index += offset;
-        size = 100 + itemHeight * offset;
+        index += NumItems;
+        size = mTitleContainerSize + itemHeight * NumItems;
+
+        Size = size;
+
+        GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
+
+        TopPos = topPos;
+
+        if (isReversePos)
+        {
+            TopPos += size;
+        }
+
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, TopPos);
+
+        mDateTitleContainer.SetAsLastSibling();
+    }
+
+    public void DoUpdate(float topBorder)
+    {
+        float titleContainerPos = 0.0f;
+        if (topBorder >= TopPos)
+        {
+            titleContainerPos = 0.0f;
+        }
+        else if (topBorder <= BottomPos + mTitleContainerSize)
+        {
+            titleContainerPos = -Size + mTitleContainerSize;
+        }
+        else
+        {
+            titleContainerPos = topBorder - TopPos;
+        }
+
+        mDateTitleContainer.anchoredPosition = new Vector2(0.0f, titleContainerPos);
     }
 }
